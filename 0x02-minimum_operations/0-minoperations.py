2f1ed68da88171b53e9640c74cf89abb260da26e
sel6 <kabodshekinah@gmail.com>
2022-04-21 07:50:47 +0300

    fix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
+""" method that calculates the fewest operaetions
+needed to result in exaxtly n and h characters"""
+
+
 def minOperations(n):
-    """return minimum number of operations needed"""
-    if (n == 1):
+    """
+    Args: number of operations
+    returns an integer
+    """
+    if n <= 1:
         return 0
-    count = 0
-    i = 2
-    while n > 1:
-        while n % i == 0:
-            count = count + i
-            n = n/i
-        i = i+1
-    return count
-  
+    for i in range(2, n + 1):
+        if n % i == 0:
+            return minOperations(int(n / i)) + i
