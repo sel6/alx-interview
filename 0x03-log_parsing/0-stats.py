a36fd61c93cd64ee2489b505bbb1975e8a9280ee
sel6 <kabodshekinah@gmail.com>
2022-05-01 11:59:06 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,69 +1,37 @@
 #!/usr/bin/python3
-""" read stdin line by line and computer metrics
-"""
+'''a script that reads stdin line by line and computes metrics'''
+
+
 import sys
-import re
-import signal
-from collections import OrderedDict
 
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
-def search_items(line, s):
-    """ search items to position"""
-    regexu = r"\s\d{3}\s\d{1,}"
-    txt = re.search(regexu, line)
-    word = txt.group()
-    word = word[1:]
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
 
-    regexd = r"\d{3}\s"
-    left = re.search(regexd, word)
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-    code = left.group()
-    code = code[:-1]
+except Exception as err:
+    pass
 
-    regext = r"\s\d{1,}"
-    right = re.search(regext, word)
-
-    size = right.group()
-    size = size[1:]
-    size = int(size)
-
-    add_code(code, s)
-
-    return size
-
-def add_code(code, codes):
-    """count the status code"""
-    try:
-        codes[code] += 1
-    except KeyError:
-        pass
-
-def print_all(stat):
-    """ Print all """
-    stat = OrderedDict(stat)
-
-    for key, values in stat.items():
-        if value is not 0:
-            print("{}: {}".format(key, value))
-
-if __name__ == "__main_":
-    status = {"200":0, "301":0, "400":0, "401":0,
-            "403":0, "404":0,"405":0,"500":0}
-    file_size = 0
-    i = 0
-
-    try:
-        for lines in sys.stdin:
-            file_size += search_items(lines, status)
-
-            if i is not 0 and i % 9 == 0:
-                print("Files size: {:d}".format(file_size))
-                print_all(status)
-
-            i += 1
-    except KeyboardInterrupt:
-        pass
-    finally:
-        print("File size: {:d}".format(file_size))
-        print_all(status)
-        sys.exit(0)
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
